cd75def76e2a1e86489edf165c6e891de75b4649
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-06 15:45:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ locals {
   secret = azurerm_key_vault_secret.secret.value
 }
 
-resource "azurerm_resource_group" "harsha-rg" {
-  name     = "harsha-rg"
+resource "azurerm_resource_group" "bhaskar-rg" {
+  name     = "bhaskar-rg"
   location = "EAST US"
 }
 
-resource "azurerm_resource_group_template_deployment" "harshadeploy" {
-  name                = "harshadeploy"
-  resource_group_name = azurerm_resource_group.harsha-rg.name
+resource "azurerm_resource_group_template_deployment" "bhaskardeploy" {
+  name                = "bhaskardeploy"
+  resource_group_name = azurerm_resource_group.bhaskar-rg.name
   deployment_mode = "Incremental"
   parameters_content = jsonencode({
     "secret" = {
@@ -27,24 +27,24 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                 "description": "password"
             }
         },
-        "virtualMachines_harsha1512_name": {
-            "defaultValue": "harsha1612",
+        "virtualMachines_bhaskar1512_name": {
+            "defaultValue": "bhaskar1612",
             "type": "String"
         },
-        "networkInterfaces_harsha1512213_name": {
-            "defaultValue": "harsha1612213",
+        "networkInterfaces_bhaskar1512213_name": {
+            "defaultValue": "bhaskar1612213",
             "type": "String"
         },
-        "publicIPAddresses_harsha1512_ip_name": {
-            "defaultValue": "harsha1612-ip",
+        "publicIPAddresses_bhaskar1512_ip_name": {
+            "defaultValue": "bhaskar1612-ip",
             "type": "String"
         },
-        "virtualNetworks_harsha1512_vnet_name": {
-            "defaultValue": "harsha1612-vnet",
+        "virtualNetworks_bhaskar1512_vnet_name": {
+            "defaultValue": "bhaskar1612-vnet",
             "type": "String"
         },
-        "networkSecurityGroups_harsha1512_nsg_name": {
-            "defaultValue": "harsha1612-nsg",
+        "networkSecurityGroups_bhaskar1512_nsg_name": {
+            "defaultValue": "bhaskar1612-nsg",
             "type": "String"
         }
         
@@ -54,13 +54,13 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/networkSecurityGroups",
             "apiVersion": "2023-04-01",
-            "name": "[parameters('networkSecurityGroups_harsha1512_nsg_name')]",
+            "name": "[parameters('networkSecurityGroups_bhaskar1512_nsg_name')]",
             "location": "eastus",
             "properties": {
                 "securityRules": [
                     {
                         "name": "RDP",
-                        "id": "[resourceId('Microsoft.Network/networkSecurityGroups/securityRules', parameters('networkSecurityGroups_harsha1512_nsg_name'), 'RDP')]",
+                        "id": "[resourceId('Microsoft.Network/networkSecurityGroups/securityRules', parameters('networkSecurityGroups_bhaskar1512_nsg_name'), 'RDP')]",
                         "type": "Microsoft.Network/networkSecurityGroups/securityRules",
                         "properties": {
                             "protocol": "TCP",
@@ -83,7 +83,7 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/publicIPAddresses",
             "apiVersion": "2023-04-01",
-            "name": "[parameters('publicIPAddresses_harsha1512_ip_name')]",
+            "name": "[parameters('publicIPAddresses_bhaskar1512_ip_name')]",
             "location": "eastus",
             "sku": {
                 "name": "Standard",
@@ -100,7 +100,7 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/virtualNetworks",
             "apiVersion": "2023-04-01",
-            "name": "[parameters('virtualNetworks_harsha1512_vnet_name')]",
+            "name": "[parameters('virtualNetworks_bhaskar1512_vnet_name')]",
             "location": "eastus",
             "properties": {
                 "addressSpace": {
@@ -111,7 +111,7 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                 "subnets": [
                     {
                         "name": "default",
-                        "id": "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_harsha1512_vnet_name'), 'default')]",
+                        "id": "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_bhaskar1512_vnet_name'), 'default')]",
                         "properties": {
                             "addressPrefix": "10.1.0.0/24",
                             "delegations": [],
@@ -128,10 +128,10 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Compute/virtualMachines",
             "apiVersion": "2023-03-01",
-            "name": "[parameters('virtualMachines_harsha1512_name')]",
+            "name": "[parameters('virtualMachines_bhaskar1512_name')]",
             "location": "eastus",
             "dependsOn": [
-                "[resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_harsha1512213_name'))]"
+                "[resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_bhaskar1512213_name'))]"
             ],
             "properties": {
                 "hardwareProfile": {
@@ -153,8 +153,8 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                     "diskControllerType": "SCSI"
                 },
                 "osProfile": {
-                    "computerName": "[parameters('virtualMachines_harsha1512_name')]",
-                    "adminUsername": "harsha",
+                    "computerName": "[parameters('virtualMachines_bhaskar1512_name')]",
+                    "adminUsername": "bhaskar",
                     "adminPassword": "[parameters('secret')]",
                     "windowsConfiguration": {
                         "provisionVMAgent": true,
@@ -172,7 +172,7 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                 "networkProfile": {
                     "networkInterfaces": [
                         {
-                            "id": "[resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_harsha1512213_name'))]",
+                            "id": "[resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_bhaskar1512213_name'))]",
                             "properties": {
                                 "deleteOption": "Detach"
                             }
@@ -189,9 +189,9 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/networkSecurityGroups/securityRules",
             "apiVersion": "2023-04-01",
-            "name": "[concat(parameters('networkSecurityGroups_harsha1512_nsg_name'), '/RDP')]",
+            "name": "[concat(parameters('networkSecurityGroups_bhaskar1512_nsg_name'), '/RDP')]",
             "dependsOn": [
-                "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_harsha1512_nsg_name'))]"
+                "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_bhaskar1512_nsg_name'))]"
             ],
             "properties": {
                 "protocol": "TCP",
@@ -211,9 +211,9 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/virtualNetworks/subnets",
             "apiVersion": "2023-04-01",
-            "name": "[concat(parameters('virtualNetworks_harsha1512_vnet_name'), '/default')]",
+            "name": "[concat(parameters('virtualNetworks_bhaskar1512_vnet_name'), '/default')]",
             "dependsOn": [
-                "[resourceId('Microsoft.Network/virtualNetworks', parameters('virtualNetworks_harsha1512_vnet_name'))]"
+                "[resourceId('Microsoft.Network/virtualNetworks', parameters('virtualNetworks_bhaskar1512_vnet_name'))]"
             ],
             "properties": {
                 "addressPrefix": "10.1.0.0/24",
@@ -225,19 +225,19 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
         {
             "type": "Microsoft.Network/networkInterfaces",
             "apiVersion": "2023-04-01",
-            "name": "[parameters('networkInterfaces_harsha1512213_name')]",
+            "name": "[parameters('networkInterfaces_bhaskar1512213_name')]",
             "location": "eastus",
             "dependsOn": [
-                "[resourceId('Microsoft.Network/publicIPAddresses', parameters('publicIPAddresses_harsha1512_ip_name'))]",
-                "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_harsha1512_vnet_name'), 'default')]",
-                "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_harsha1512_nsg_name'))]"
+                "[resourceId('Microsoft.Network/publicIPAddresses', parameters('publicIPAddresses_bhaskar1512_ip_name'))]",
+                "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_bhaskar1512_vnet_name'), 'default')]",
+                "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_bhaskar1512_nsg_name'))]"
             ],
             "kind": "Regular",
             "properties": {
                 "ipConfigurations": [
                     {
                         "name": "ipconfig1",
-                        "id": "[concat(resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_harsha1512213_name')), '/ipConfigurations/ipconfig1')]",
+                        "id": "[concat(resourceId('Microsoft.Network/networkInterfaces', parameters('networkInterfaces_bhaskar1512213_name')), '/ipConfigurations/ipconfig1')]",
                         "etag": "W/\"875b6ef9-066d-4a97-b7c5-9a2f02f196fb\"",
                         "type": "Microsoft.Network/networkInterfaces/ipConfigurations",
                         "properties": {
@@ -245,13 +245,13 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                             "privateIPAddress": "10.1.0.4",
                             "privateIPAllocationMethod": "Dynamic",
                             "publicIPAddress": {
-                                "id": "[resourceId('Microsoft.Network/publicIPAddresses', parameters('publicIPAddresses_harsha1512_ip_name'))]",
+                                "id": "[resourceId('Microsoft.Network/publicIPAddresses', parameters('publicIPAddresses_bhaskar1512_ip_name'))]",
                                 "properties": {
                                     "deleteOption": "Detach"
                                 }
                             },
                             "subnet": {
-                                "id": "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_harsha1512_vnet_name'), 'default')]"
+                                "id": "[resourceId('Microsoft.Network/virtualNetworks/subnets', parameters('virtualNetworks_bhaskar1512_vnet_name'), 'default')]"
                             },
                             "primary": true,
                             "privateIPAddressVersion": "IPv4"
@@ -265,7 +265,7 @@ resource "azurerm_resource_group_template_deployment" "harshadeploy" {
                 "enableIPForwarding": false,
                 "disableTcpStateTracking": false,
                 "networkSecurityGroup": {
-                    "id": "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_harsha1512_nsg_name'))]"
+                    "id": "[resourceId('Microsoft.Network/networkSecurityGroups', parameters('networkSecurityGroups_bhaskar1512_nsg_name'))]"
                 },
                 "nicType": "Standard",
                 "auxiliaryMode": "None",
