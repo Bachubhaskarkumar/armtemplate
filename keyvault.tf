651e571a9ae8b9422918e84c9f3a0859a688fc5d
Bachubhaskarkumar <80734795+Bachubhaskarkumar@users.noreply.github.com>
2023-10-10 15:57:47 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -2,8 +2,8 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "vault" {
   name                        = "bhaskararm"
-  location                    = azurerm_resource_group.bhaskar-rg.location
-  resource_group_name         = azurerm_resource_group.bhaskar-rg.name
+  location                    = azurerm_resource_group.bhaskar-rg1.location
+  resource_group_name         = azurerm_resource_group.bhaskar-rg1.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
